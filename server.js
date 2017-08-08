cda6ace58747211de56aa23b6c7ea259f68d66e4
Arjun <lallarjun@gmail.com>
2017-08-08 00:42:16 +0000

    fixed cors

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const PORT = process.env.PORT|| 3000;
 const foodRoutes = require("./routes/food");
 const cors = require('cors');
 
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use("/food", foodRoutes());
-app.use(cors());
 
 app.listen(PORT, () => {
   console.log("Listening on port " + PORT);
